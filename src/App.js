f39096c5a5ea088419bc0621012718a5a1ff19e4
supra1nova <supra1nova@gmail.com>
2022-06-08 14:48:35 +0900

    id, title, body가 변하도록 개선

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { useState } from 'react'; // 이런 애들을 Hook이라고 부른다.
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
-function Header() {
+function Header(props) {
+  console.log(props);
   return (
     <header>
       <h1>
-        <a href="/">WWW</a>
+        <a
+          href="/"
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect();
+          }}
+        >
+          WWW
+        </a>
       </h1>
     </header>
   );
@@ -17,7 +27,15 @@ function Nav(props) {
   const liTags = props.data.map((e) => {
     return (
       <li key={e.id}>
-        <a href="{'/read/' + e.id}">{e.title}</a>
+        <a
+          href="{'/read/' + e.id}"
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect(e.id);
+          }}
+        >
+          {e.title}
+        </a>
       </li>
     );
   });
@@ -38,17 +56,40 @@ function Article(props) {
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  console.log(mode, id);
   const topics = [
     { id: 1, title: 'html', body: 'html is...' },
     { id: 2, title: 'css', body: 'css is...' },
   ];
+  let content = null;
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB !" />;
+  } else if (mode === 'READ') {
+    const topic = topics.filter(e => e.id === id)[0];
+    content = <Article title={topic.title} body={`Hello, ${topic.body} !`} />;
+    // content = <Article title="READ" body="Hello, READ !" />;
+  }
   return (
     <div>
-      <Header />
-      <Nav data={topics} />
-      <Article title="Welcome" body="Hello, WEB !" />
+      <Header
+        onSelect={() => {
+          setMode('WELCOME');
+        }}
+      />
+      <Nav
+        data={topics}
+        onSelect={(id) => {
+          setMode('READ');
+          setId(id);
+        }}
+      />
+      {content}
       <ButtonGroup>
-        <Button>Create</Button>
+        <Button variant="outlined" onClick={() => alert('create!')}>
+          Create
+        </Button>
         <Button>Update</Button>
       </ButtonGroup>
       <Button variant="outlined">Delete</Button>
