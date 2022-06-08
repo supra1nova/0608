2cadf963209cdbde19fd397c48b2aedea49cc975
supra1nova <supra1nova@gmail.com>
2022-06-08 11:35:21 +0900

    button group 도입

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WWW</a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  console.log('props', props.data);
+  const liTags = props.data.map((e) => {
+    return (
+      <li key={e.id}>
+        <a href="{'/read/' + e.id}">{e.title}</a>
+      </li>
+    );
+  });
+  return (
+    <nav>
+      <ol>{liTags}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 
 function App() {
+  const topics = [
+    { id: 1, title: 'html', body: 'html is...' },
+    { id: 2, title: 'css', body: 'css is...' },
+  ];
   return (
-    <div className='App'>
-      <h1>Hello World!!</h1>
+    <div>
+      <Header />
+      <Nav data={topics} />
+      <Article title="Welcome" body="Hello, WEB !" />
+      <ButtonGroup>
+        <Button>Create</Button>
+        <Button>Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
